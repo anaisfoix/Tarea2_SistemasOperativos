d968bc9d7daf9297b400825c75935e64546873fc
anaisfoix <foixanais@gmail.com>
2021-11-26 18:05:57 -0300

    Casi listo?

--- tarea.sh
+++ tarea.sh
@@ -7,19 +7,19 @@ if [[ $# -eq 4 ]];then
 	
 	archivo=$(find / -user $2 -name $1)
 	
-	if [[ $archivo= "" ]];then
+	if [[ $archivo = "" ]];then
 		echo "Error el archivo ingresado no se encuentra"
 	 
 	else
 		echo "El archivo fue encontrado se procede a realizar el cambio"
-		opcion=$(ls -l testing | awk '{print $3}')
-		echo "Opcion ingresada: "opcion
-		if [[ $opcion = '-p' ]]
-			chmod 
-		elif [[ $opcion = '-d' ]]
-			chown nuevoUsuario /home/Tarea2_SistemasOperativos/tarea.sh
-		elif [[ $opcion = '-g' ]]
-			chgrp 
+		opcion=$()
+		echo $opcion
+		if [[ $opcion = "-p" ]]; then
+			chmod $4 /home/Tarea2_SistemasOperativos/tarea.sh
+		elif [[ $opcion = "-d" ]]; then
+			chown $4 /home/Tarea2_SistemasOperativos/tarea.sh
+		elif [[ $opcion = "-g" ]]; then
+			chgrp $4 /home/Tarea2_SistemasOperativos/tarea.sh
 		else
 			echo "Opcion no valida"
 		fi
