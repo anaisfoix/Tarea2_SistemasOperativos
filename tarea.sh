0c316d84830fd3dbe6f1013815bb8cc2c2d34fc4
anaisfoix <foixanais@gmail.com>
2021-11-26 17:33:51 -0300

    Implementacion if

--- tarea.sh
+++ tarea.sh
@@ -7,11 +7,23 @@ if [[ $# -eq 4 ]];then
 	
 	archivo=$(find / -user $2 -name $1)
 	
-	if [[ $archivo="" ]];then
+	if [[ $archivo= "" ]];then
 		echo "Error el archivo ingresado no se encuentra"
 	 
 	else
 		echo "El archivo fue encontrado se procede a realizar el cambio"
+		opcion=$(ls -l testing | awk '{print $3}')
+		echo "Opcion ingresada: "opcion
+		if [[ $opcion = '-p' ]]
+			chmod 
+		elif [[ $opcion = '-d' ]]
+			chown nuevoUsuario /home/Tarea2_SistemasOperativos/tarea.sh
+		elif [[ $opcion = '-g' ]]
+			chgrp 
+		else
+			echo "Opcion no valida"
+		fi
+		
 	fi
 else
 	echo "No hay parametros suficientes"
